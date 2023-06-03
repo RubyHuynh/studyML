7d38fc29cb01cdbd050c2166e6a42a79f2ac7282
RubyHuynh <hthn.oo@gmail.com>
2023-06-03 16:24:01 +0000

    number, cast, string1

--- hello_world/helloworld.py
+++ hello_world/helloworld.py
@@ -122,20 +122,64 @@ x = None
 print ("None",  type(x), x)
 
 
+# numbers
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+
+import random
+
+print(random.randrange(1, 10))
+
+
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
 
 
 
+a ="hell\"\o"
+b = '''sdjadkad
+sdasdjdhaksd"""""F"DFSFDSF"
+adsadhasjk'''
+c = """sdjasd
+122434234
+t54353rreee'''"""
+print(a, b, c)
+print(len(b))
+
+print ("sdas" in b)
+for i in a:
+	print(i)
 
 
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
 
 
-
-
-
-
-
-
-
+print ("expensive" not in txt)
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
 
 
 
